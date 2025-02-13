4115c578a3bbf6493c4b59394a7189342f1bb0c5
Anthony <91166746+anthonyjV1@users.noreply.github.com>
2025-02-12 20:26:25 -0500

    reddit bot

--- main.py
+++ main.py
@@ -1,19 +1,53 @@
 import praw
 import config
+import time
+import os
+import requests
 
 def bot_login():
+    print("Loggin in...")
     r = praw.Reddit(username = config.username,
             password = config.password,
             client_id = config.client_id,
             client_secret = config.client_secret,
-            user_agent = "dudelookbehindu's dog comment responder v0.1" )
+            user_agent = "dudelookbehindu's joke comment responder v0.1" )
+    print("Logged in!")
     return r
 
-def run_bot(r):
-    for comment in r.subreddit('test').comments(limit = 25):
-        if "zyyz" in comment.body:
-            print ("String found!")
-            comment.reply("I love logs!")
+def run_bot(r, comments_replied_to):
+    print("Obtaining 25 comments")
 
-r = bot_login()
-run_bot(r)
+    for comment in r.subreddit('test').comments(limit = 10):
+        if "!joke" in comment.body and comment.id not in comments_replied_to and comment.author != r.user.me():
+            print ("String with \"!joke\"found!"+ comment.id)
+            comment_reply = "You requested a Chuck Norris joke! Here it is: \n\n"
+            #joke = requests.get("http://api.icndb.com/jokes/random").json()['value']['joke']
+            comment_reply += ">" + "You suck lol"
+            #comment_reply += "\n\nThis joke came from [ICNDb.com](http://icndb.com)"
+            comment.reply(comment_reply)
+            print("Replied to comment" + comment.id)
+            comments_replied_to.append(comment.id)
+
+            with open("comments_replied_to.txt", "a") as f:
+                f.write(comment.id + "\n")
+
+
+    print("Sleeping for 10 seconds...")        
+    time.sleep(10)
+
+def get_saved_comments():
+    if not os.path.isfile("comments_replied_to.txt"):
+        comments_replied_to = []
+    else: 
+        with open("comments_replied_to.txt", "r") as f:
+            comments_replied_to = f.read()
+            comments_replied_to = comments_replied_to.split("\n")
+            comments_replied_to = list(filter(None, comments_replied_to))
+    return comments_replied_to
+
+r= bot_login()
+comments_replied_to = get_saved_comments()
+print(comments_replied_to)
+
+while True:
+    run_bot(r, comments_replied_to)
